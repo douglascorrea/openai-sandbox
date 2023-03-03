297d9535a9311154de4122f5b450caa4a78daae8
Douglas Silvio Correa <me@douglascorrea.io>
2023-03-03 18:40:49 -0300

    Using .env files

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import openai
+from dotenv import load_dotenv
 
+# Load environment variables from the .env file
+load_dotenv()
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
